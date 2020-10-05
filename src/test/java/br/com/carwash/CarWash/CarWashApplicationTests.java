50a1a83290fad6b0023178456f1e28959af293ac
Daniel Aguiar <daniel.aguiar@zup.com.br>
2020-10-05 18:13:32 -0300

    Implements base project
    Signed-off-by: Daniel Aguiar <daniel.aguiar@zup.com.br>

---
+++ src/test/java/br/com/carwash/CarWash/CarWashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carwash.CarWash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
